09c7792040ed0118becee6b474cde2e4b59e95ed
NBor <nborle@ualberta.ca>
2013-05-20 16:10:39 -0600

    translated LineSource and created a Colors enum. created the parser in order to create the colors enum.

---
+++ src/source/LineSource.py
@@ -0,0 +1,30 @@
+'''
+Created on 2013-05-16
+
+@author: Neil
+'''
+
+from Source import Source
+from utils.Colors import colors
+
+class LineSource(Source):
+    '''
+    classdocs
+    '''
+    GC_verticies = []
+    ra_decs = []
+    line_width = None
+
+    def __init__(self, new_color=colors.WHITE, gcvs, lw=1.5):
+        '''
+        Constructor
+        '''
+        Source.__init__(self, new_color)
+        self.line_width = lw
+        self.GC_verticies = gcvs
+        
+if __name__ == "__main__":
+    '''
+    For debugging purposes
+    Ready for testing
+    '''
---
+++ src/utils/ColorParser.py
@@ -0,0 +1,24 @@
+'''
+Created on 2013-05-20
+
+@author: Neil
+'''
+import sys
+
+handle = open(sys.argv[1], 'r')
+handle2 = open(sys.argv[2], 'w')
+
+
+for line in handle.readlines():
+	'''
+	used for parsing color xml file from 
+	http://stackoverflow.com/questions/3769762/android-color-xml-resource-file
+	'''
+	split_line = line.split('"')
+	color_code_int = split_line[2].split('<')[0]
+	color_code = color_code_int.split('#')[1]
+	print split_line[1].upper() + "=" + color_code +", \\"
+	handle2.write(split_line[1].upper() + "=0x" + color_code +", \\" + "\n")
+
+handle.close()
+handle2.close()

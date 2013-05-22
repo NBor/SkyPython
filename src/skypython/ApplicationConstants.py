89008295161ee8b30e851896051a98fe5648dc64
NBor <nborle@ualberta.ca>
2013-05-22 16:02:52 -0600

    Made ApplicationConstants

---
+++ src/skypython/ApplicationConstants.py
@@ -0,0 +1,13 @@
+'''
+Created on 2013-05-22
+
+@author: Neil
+'''
+
+from units.Vector3 import Vector3
+
+APP_NAME = "Stardroid"
+#Default value for 'south' in phone coords when the app starts
+INITIAL_SOUTH = Vector3(0, -1, 0)
+#Default value for 'down' in phone coords when the app starts
+INITIAL_DOWN = Vector3(0, -1, -9)

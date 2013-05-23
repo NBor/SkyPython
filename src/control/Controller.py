5d8b43b69282d72082c4413c17dbd9c850520403
NBor <nborle@ualberta.ca>
2013-05-23 14:53:52 -0600

    Made really really simple controller for inheritance to mimic the java code

---
+++ src/control/Controller.py
@@ -0,0 +1,24 @@
+'''
+Created on 2013-05-23
+
+@author: Neil
+'''
+
+class Controller(object):
+    '''
+    classdocs
+    '''
+    model = None
+    enabled = True
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+if __name__ == "__main__":
+    '''
+    For debugging purposes
+    Ready for testing
+    '''
